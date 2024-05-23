8872440d33821e038ff33cd4bacf58b37fde7150
Ajay Gol <158291449+yashpatel9090@users.noreply.github.com>
2024-05-23 15:19:56 +0530

    remove dark mode from navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     }
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container">
                     <Link className="navbar-brand" to="/">iNotebook</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
